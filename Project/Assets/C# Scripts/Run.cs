b5bb3973d6104c9af62b9241ab843ef928e1d6c8
user2243961 <409652@student.sit.ac.nz>
2016-09-12 10:50:13 +1200

    Initial
    Initial

---
+++ Project/Assets/C# Scripts/Run.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Run : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        {
+            float move = Input.GetAxis("Horizontal");
+            anim.SetFloat("Speed", move);
+        }
+	}
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.right * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, -180);
+        }
+    }
+}
