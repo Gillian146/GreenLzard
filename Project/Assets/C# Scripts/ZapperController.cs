9d7c0bfc64a756bed1fa3388376fe9f3502c7845
Gemmy <2015009756@student.sit.ac.nz>
2016-10-19 10:30:03 +1300

    ScoreManger update
    update your score

--- Project/Assets/C# Scripts/ZapperController.cs
+++ Project/Assets/C# Scripts/ZapperController.cs
@@ -2,13 +2,14 @@
 using System.Collections;
 
 public class ZapperController : MonoBehaviour {
-
+	private ScoreManager sm;
     public BugManager bugManager;
 
     private AudioSource deathSound;
 
     // Use this for initialization
     void Start () {
+		sm = FindObjectOfType<ScoreManager>();
         bugManager = FindObjectOfType<BugManager>();
         deathSound = GetComponent<AudioSource>();
     }
@@ -24,6 +25,8 @@ public class ZapperController : MonoBehaviour {
         {
             deathSound.Play();
             bugManager.KillBug(other);
+			sm.score += 10;
+
         }
     }
 }
